31b83a9124d01b4a903a689c674e131266ed7feb
Navelogic <weslleysantoszbd@gmail.com>
2025-04-13 18:16:47 -0300

    Add ProductionStatusEnum to define various production statuses

---
+++ Backend/api/src/main/java/com/github/navelogic/api/Enum/ProductionStatusEnum.java
@@ -0,0 +1,11 @@
+package com.github.navelogic.api.Enum;
+
+public enum ProductionStatusEnum {
+    CONCEPT,
+    PRE_PRODUCTION,
+    PRODUCTION,
+    POST_PRODUCTION,
+    READY_FOR_RELEASE,
+    RELEASED,
+    CANCELED
+}
