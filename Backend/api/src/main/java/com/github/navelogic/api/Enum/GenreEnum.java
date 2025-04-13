d2ac7ee2bf504f8d35bed12e0253e79d7aee70cd
Navelogic <weslleysantoszbd@gmail.com>
2025-04-13 18:15:18 -0300

    Add GenreEnum to define various film genres

---
+++ Backend/api/src/main/java/com/github/navelogic/api/Enum/GenreEnum.java
@@ -0,0 +1,18 @@
+package com.github.navelogic.api.Enum;
+
+public enum GenreEnum {
+    ACTION,
+    COMEDY,
+    DRAMA,
+    SCIFI,
+    HORROR,
+    DOCUMENTARY,
+    ROMANCE,
+    FANTASY,
+    ADVENTURE,
+    MYSTERY,
+    CRIME,
+    MUSICAL,
+    WESTERN,
+    ANIMATION
+}
