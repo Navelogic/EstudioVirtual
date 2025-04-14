0a31a19df72caee651187ae254a6db18ccc8239d
Navelogic <weslleysantoszbd@gmail.com>
2025-04-13 23:52:33 -0300

    Add repositories for Contract, Crew, and Studio models

---
+++ Backend/api/src/main/java/com/github/navelogic/api/Repository/CrewRepository.java
@@ -0,0 +1,7 @@
+package com.github.navelogic.api.Repository;
+
+import com.github.navelogic.api.Model.Crew;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CrewRepository extends JpaRepository<Crew, Long> {
+}
---
+++ Backend/api/src/main/java/com/github/navelogic/api/Repository/StudioRepository.java
@@ -0,0 +1,7 @@
+package com.github.navelogic.api.Repository;
+
+import com.github.navelogic.api.Model.Studio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudioRepository extends JpaRepository<Studio, Long> {
+}
