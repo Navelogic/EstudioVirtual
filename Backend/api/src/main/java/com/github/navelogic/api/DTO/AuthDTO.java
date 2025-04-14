633304687eb9c9ab356a4f33af7d62c54400d88e
Navelogic <weslleysantoszbd@gmail.com>
2025-04-14 10:49:16 -0300

    Add AuthDTO class for user authentication data transfer

---
+++ Backend/api/src/main/java/com/github/navelogic/api/DTO/AuthDTO.java
@@ -0,0 +1,20 @@
+package com.github.navelogic.api.DTO;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthDTO {
+
+    @Schema(description = "Email do usuário", example = "joao.silva@email.com")
+    private String email;
+
+    @Schema(description = "Senha do usuário", example = "JoãoSenha@1234")
+    private String password;
+}
