6970cdd8a558f25b9dfd6cb023433204b8068342
Navelogic <weslleysantoszbd@gmail.com>
2025-04-14 10:40:50 -0300

    Add SecurityConfig class to configure security settings and filter chain

---
+++ Backend/api/src/main/java/com/github/navelogic/api/Security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.github.navelogic.api.Security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final SecurityFilter securityFilter;
+
+    private static final String[] SWAGGER_WHITELIST = {
+            "/v3/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            "/swagger-ui/**",
+            "/v3/api-docs/swagger-config"
+    };
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/auth").permitAll()
+                            .requestMatchers("/auth/").permitAll()
+                            .requestMatchers("/auth/login").permitAll()
+                            .requestMatchers("/auth/register").permitAll()
+                            .requestMatchers(SWAGGER_WHITELIST).permitAll();
+
+                    auth.anyRequest().authenticated();
+                })
+                .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
