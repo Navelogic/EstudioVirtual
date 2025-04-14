a04110aafd70a2939f70a767217b8c816e830fba
Navelogic <weslleysantoszbd@gmail.com>
2025-04-14 10:51:40 -0300

    Add AuthResponseDTO class for handling authentication response data

---
+++ Backend/api/src/main/java/com/github/navelogic/api/DTO/AuthResponseDTO.java
@@ -0,0 +1,21 @@
+package com.github.navelogic.api.DTO;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponseDTO {
+
+    @Schema(description = "Token de acesso gerado para o usuário", example = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
+    private String acessToken;
+
+    @Schema(description = "Papel do usuário no sistema", example = "ROLE_USER")
+    private String role;
+
+}
