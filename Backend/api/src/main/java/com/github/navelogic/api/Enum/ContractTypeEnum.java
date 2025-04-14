c6e8faaf7111f76c60d0fb50c34d1a77586815da
Navelogic <weslleysantoszbd@gmail.com>
2025-04-13 23:20:32 -0300

    Add enums for ContractType and CrewRole

---
+++ Backend/api/src/main/java/com/github/navelogic/api/Enum/ContractTypeEnum.java
@@ -0,0 +1,8 @@
+package com.github.navelogic.api.Enum;
+
+public enum ContractTypeEnum {
+        EXCLUSIVE,
+        FREELANCE,
+        TIME_BASED,
+        PRODUCTION_BASED
+}
---
+++ Backend/api/src/main/java/com/github/navelogic/api/Enum/CrewRoleEnum.java
@@ -0,0 +1,13 @@
+package com.github.navelogic.api.Enum;
+
+public enum CrewRoleEnum {
+    DIRECTOR,
+    SCREENWRITER,
+    CINEMATOGRAPHER,
+    EDITOR,
+    COMPOSER,
+    ART_DIRECTOR,
+    COSTUME_DESIGNER,
+    SOUND_DESIGNER,
+    VISUAL_EFFECTS_SUPERVISOR
+}
