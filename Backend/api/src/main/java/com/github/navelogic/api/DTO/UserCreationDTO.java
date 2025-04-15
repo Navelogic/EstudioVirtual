b2a1ffb33d544bfc488d3ec7a15d1aac42588935
Navelogic <weslleysantoszbd@gmail.com>
2025-04-15 11:58:17 -0300

    adiciona validações para os campos username, email e password no UserCreationDTO

--- Backend/api/src/main/java/com/github/navelogic/api/DTO/UserCreationDTO.java
+++ Backend/api/src/main/java/com/github/navelogic/api/DTO/UserCreationDTO.java
@@ -1,6 +1,10 @@
 package com.github.navelogic.api.DTO;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,11 +17,16 @@ import lombok.NoArgsConstructor;
 public class UserCreationDTO {
 
     @Schema(description = "Username usuário", example = "Navelogic")
+    @NotBlank(message = "O username não pode ser vazio.")
     private String username;
 
     @Schema(description = "Email do usuário", example = "navelogic@email.com")
+    @NotBlank(message = "O email não pode ser vazio")
+    @Email(message = "O email deve ser válido")
     private String email;
 
     @Schema(description = "Senha do usuário", example = "123456789QWERTYUIOP")
+    @NotBlank(message = "A senha não pode ser vazia")
+    @Size(min = 4, message = "A senha deve ter no mínimo 4 caracteres")
     private String password;
 }
