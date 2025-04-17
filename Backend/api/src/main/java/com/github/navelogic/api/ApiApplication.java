47851cb2a4146d36da0396bb943aa0599e56ccca
Navelogic <weslleysantoszbd@gmail.com>
2025-04-16 23:23:32 -0300

    adiciona configuração de segurança para autenticação JWT na aplicação

--- Backend/api/src/main/java/com/github/navelogic/api/ApiApplication.java
+++ Backend/api/src/main/java/com/github/navelogic/api/ApiApplication.java
@@ -1,9 +1,13 @@
 package com.github.navelogic.api;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityScheme(name = "jwt_auth", scheme = "bearer", bearerFormat = "JWT", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class ApiApplication {
 
 	public static void main(String[] args) {
