b0aa70b38b9dccd44d5e3f4ca03a82447aa8aeff
Navelogic <weslleysantoszbd@gmail.com>
2025-04-18 17:29:05 -0300

    adiciona AccessDeniedException para tratamento de erros 403

---
+++ Backend/api/src/main/java/com/github/navelogic/api/Exception/AccessDeniedException.java
@@ -0,0 +1,15 @@
+package com.github.navelogic.api.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.FORBIDDEN)
+public class AccessDeniedException extends RuntimeException {
+  public AccessDeniedException(String message) {
+    super(message);
+  }
+
+  public AccessDeniedException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
