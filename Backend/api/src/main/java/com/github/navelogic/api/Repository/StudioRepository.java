9f21dd93ef3066a8f34502cf7b3d0a8802be5876
Navelogic <weslleysantoszbd@gmail.com>
2025-04-18 18:29:15 -0300

    adiciona método findByOwnerId à interface StudioRepository

--- Backend/api/src/main/java/com/github/navelogic/api/Repository/StudioRepository.java
+++ Backend/api/src/main/java/com/github/navelogic/api/Repository/StudioRepository.java
@@ -1,7 +1,14 @@
 package com.github.navelogic.api.Repository;
 
 import com.github.navelogic.api.Model.Studio;
+import com.github.navelogic.api.Model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
 public interface StudioRepository extends JpaRepository<Studio, Long> {
+    Optional<UserModel> findByOwnerId(UUID userId);
 }
