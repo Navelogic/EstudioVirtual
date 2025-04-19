5011abb44043b36371aaf2cba1795357a8fac84e
Navelogic <weslleysantoszbd@gmail.com>
2025-04-19 15:38:58 -0300

    Adiciona endpoints CRUD ao StudioController

--- Backend/api/src/main/java/com/github/navelogic/api/Controller/StudioController.java
+++ Backend/api/src/main/java/com/github/navelogic/api/Controller/StudioController.java
@@ -1,13 +1,61 @@
 package com.github.navelogic.api.Controller;
 
+import com.github.navelogic.api.DTO.ApiResponseDTO;
+import com.github.navelogic.api.DTO.StudioCreationDTO;
+import com.github.navelogic.api.DTO.StudioResponseDTO;
+import com.github.navelogic.api.DTO.StudioUpdateDTO;
+import com.github.navelogic.api.Exception.ValidationException;
+import com.github.navelogic.api.Model.Studio;
+import com.github.navelogic.api.Service.StudioService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/studio")
 @RequiredArgsConstructor
 public class StudioController {
 
+    private final StudioService studioService;
+
+    @PostMapping("/create")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<StudioResponseDTO> createStudio(@RequestBody StudioCreationDTO studioCreationDTO, HttpServletRequest request) {
+        String userId = (String) request.getAttribute("user_id");
+        var studio = studioService.createStudio(studioCreationDTO, UUID.fromString(userId));
+        return ResponseEntity.status(HttpStatus.CREATED).body(studio);
+    }
+
+    @PutMapping("/update")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<StudioResponseDTO> updateStudio(@RequestBody StudioUpdateDTO studioUpdateDTO, HttpServletRequest request) {
+        String userId = (String) request.getAttribute("user_id");
+        StudioResponseDTO updatedStudio = studioService.updateStudio(studioUpdateDTO, UUID.fromString(userId));
+        return ResponseEntity.status(HttpStatus.OK).body(updatedStudio);
+    }
+
+    @DeleteMapping("/delete/{studioId}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Object> deleteStudio(@PathVariable Long studioId, HttpServletRequest request) {
+        try {
+            String userId = (String) request.getAttribute("user_id");
+            studioService.deleteStudio(studioId, UUID.fromString(userId));
+            return ResponseEntity.ok("Estúdio deletado com sucesso");
+        } catch (ValidationException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Object> getStudioProfile(HttpServletRequest request) {
+        String userId = (String) request.getAttribute("user_id");
+        return ResponseEntity.ok(studioService.studioProfile(UUID.fromString(userId)));
+    }
 
 }
